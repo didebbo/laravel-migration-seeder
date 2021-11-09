21cb2ba2499f6d9f726f04490787618cca16fdec
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-09 16:25:20 +0100

    first commit

---
+++ app/Http/Controllers/PackageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Package;
+use Illuminate\Http\Request;
+
+class PackageController extends Controller
+{
+    public function index()
+    {
+        dd(Package::all());
+        // dd("Hello World!");
+    }
+}
---
+++ database/migrations/2021_11_09_141931_create_packages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('code', false, true);
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('city', 50);
+            $table->string('hotel', 50);
+            $table->date('from');
+            $table->date('to');
+            $table->float('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
